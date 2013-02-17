0346691adb8d38c2a9da0d7d5882c795acdc3ea6
wwqgtxx <wwqgtxx@gmail.com>
2013-02-17 15:29:47 +0800

    bat

--- string.inc.php
+++ string.inc.php
@@ -1,10 +1,10 @@
 <?
 $str["anykeytocontinue"]="(如要继续，请按任何键)\r\n";
 
-$str["cert_notexists"]=" [!]wwqgtxx-goagent公钥并不存在! 请等候更新(较不安全)或到官网(wwqgtxx-goagent.googlecode.com)下载公钥并放置到/data/目录下\r\n";
-$str["cert_corrupted"]=" [!]wwqgtxx-goagent公钥已损毁! 请到官网(wwqgtxx-goagent.googlecode.com)重新下载公钥并覆盖到/data/目录下\r\n";
+$str["cert_notexists"]=" [!]wwqgtxx-goagent公钥并不存在! 请等候更新(较不安全)或到官网(https://code.google.com/p/wwqgtxx-goagent)或备份网站(https://code.google.com/p/wwqgtxx-wallproxy)下载公钥并放置到/data/目录下\r\n";
+$str["cert_corrupted"]=" [!]wwqgtxx-goagent公钥已损毁! 请到官网(https://code.google.com/p/wwqgtxx-goagent)或备份网站(https://code.google.com/p/wwqgtxx-wallproxy))重新下载公钥并覆盖到/data/目录下\r\n";
  
-$str["sign_incorrect"]=" [!]杂凑档签名校验错误，与谷歌服务器之间可能存在中间人攻击，请留意网络环境是否安全\r\n也有可能这只不过是中国Google更新服务器的延迟所造成\r\n";
+$str["sign_incorrect"]=" [!]HASH签名校验错误，与谷歌服务器之间可能存在中间人攻击，请留意网络环境是否安全\r\n也有可能这只不过是中国Google更新服务器的延迟所造成\r\n";
 $str["hash_sign_incorrect"]=" [!]更新过程中发现签名校验错误，与谷歌服务器之间可能存在中间人攻击，请留意网络环境是否安全\r\n";
 $str["retryin3seconds"]="3秒後重试\r\n";
 ?>
