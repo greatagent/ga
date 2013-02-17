6661752d73405695e2f1f364e0b0d06431f32f91
wwqgtxx <wwqgtxx@gmail.com>
2013-02-17 10:28:05 +0800

    bat

--- firefox.inc.php
+++ firefox.inc.php
@@ -1,3 +1,5 @@
 <?php
-exec('start ./FirefoxPortable/FirefoxPortable.exe');
+if(file_exists("./FirefoxPortable/FirefoxPortable.exe")){
+	exec('start ./FirefoxPortable/FirefoxPortable.exe "https://wwqgtxx-goagent.googlecode.com/git-history/web/ifanqiang.htm"');
+}
 ?>
