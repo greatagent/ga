8c40c2f56506ffed1b6e62286dd1847ac63442a1
wwqgtxx <wwqgtxx@gmail.com>
2013-02-17 10:31:00 +0800

    bat

--- firefox.inc.php
+++ firefox.inc.php
@@ -2,4 +2,7 @@
 if(file_exists("./FirefoxPortable/FirefoxPortable.exe")){
 	exec('start ./FirefoxPortable/FirefoxPortable.exe "https://wwqgtxx-goagent.googlecode.com/git-history/web/ifanqiang.htm"');
 }
+else{
+echo "Don't Have FirefoxPortable.";
+}
 ?>
