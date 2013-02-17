a89ff81f2ec559a9e3ee8113c256c774b3fba75e
wwqgtxx <wwqgtxx@gmail.com>
2013-02-17 15:26:11 +0800

    bat

--- update.inc.bat
+++
@@ -1,7 +0,0 @@
-:: update.inc.bat
-:: Step2 - Clean up hash file and do update
-
-echo Checking for update...
-utility\php\php.exe cleanhash.php
-utility\php\php.exe updategc.php
-utility\sleep.exe -m 1000
