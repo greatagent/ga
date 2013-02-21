4d3c80897d46645c62a725fd264ae91c057438ff
wwqgtxx <wwqgtxx@gmail.com>
2013-02-21 15:03:04 +0800

    bat

--- get-last-kown-good.inc.php
+++ get-last-kown-good.inc.php
@@ -24,6 +24,7 @@ foreach($host as $hostkey=>$hoststring){
 }
 echo "Grabbing Huhamhire-Hosts hosts:\r\n";
 $host=request("GET /git/downloads/raw/ipv4_mobile_utf8/hosts HTTP/1.0\r\nHost:{host}\r\nConnection: close\r\n\r\n","huhamhire-hosts.googlecode.com");
+echo $host;
 $host=explode("\n",str_replace("\r\n","\n",$host));
 foreach($host as $hostkey=>$hoststring){
 	//talk.google.com is special case. We cannot use it as G-Server
