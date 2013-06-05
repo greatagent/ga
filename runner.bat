36cc288b972b7f8e20eae4751f2fb36ad8bcc24e
wwqgtxx <wwqgtxx@gmail.com>
2013-06-05 09:04:38 +0800

    bat

--- runner.bat
+++ runner.bat
@@ -10,7 +10,7 @@ utility\sleep.exe -m 1000
 :: Step5 - Start GoAgent
 
 echo Starting GoAgent...
-start start-goagent.bat
+start runner2.bat
 
 :: get-last-kown-good.inc.bat
 :: Step6 - Start get-last-kown-good
